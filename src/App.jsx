83efe61a8f8268723a595a204c41ea1f525b1881
Guruprasad3n <gp76652@gmail.com>
2024-05-11 08:46:27 +0530

    Added Update Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Heading from './components/MainHeding/Heading'
-import Menu from './components/Menu/Menu'
-import Carousel from './components/Carousel/Carousel'
-import FastTitle from './components/FastTitle/FastTitle'
-import Carousel2 from './components/Carousel2/Carousel2'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Heading from "./components/MainHeding/Heading";
+import Menu from "./components/Menu/Menu";
+import Carousel from "./components/Carousel/Carousel";
+import FastTitle from "./components/FastTitle/FastTitle";
+import Carousel2 from "./components/Carousel2/Carousel2";
+import Update from "./components/Updatepart/Update";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [y, setY] = useState(0);
   useEffect(() => {
     const handleScroll = () => {
@@ -20,17 +21,17 @@ function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-
   return (
     <>
-       <Navbar y={y} />
+      <Navbar y={y} />
       <Heading />
       <Menu y={y} />
       <Carousel y={y} />
+      <Update />
       <FastTitle />
       <Carousel2 y={y} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
